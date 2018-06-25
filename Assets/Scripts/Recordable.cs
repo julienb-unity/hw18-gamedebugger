fa55b90fe61c0e893f858cc22ab01d17cd5b0d15
Neil Devine <neil@unity3d.com>
2018-06-25 17:26:01 +0200

    added recordables

---
+++ Assets/Scripts/Recordable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class Recordable
+{
+}
+public class RecordableComponent<T> : Recordable<T> where T : Component
+{
+    public virtual void OnRecord(GameObject go, Component component)
+    {
+        
+    }
+    public virtual void OnReplay(GameObject go, Component component)
+    {
+    }
+}
+
+public class Recordable<T> : Recordable where T : Object
+{
+    public virtual void OnRecord(GameObject go, T data)
+    {
+        
+    }
+    public virtual void OnReplay(GameObject go, T data)
+    {
+    }
+}
---
+++ Assets/TransformRecorder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TransformRecorder : RecordableComponent<Transform>
+{
+    [SerializeField]
+    private Vector3 position;    
+    [SerializeField]
+    private Quaternion rotation;    
+    [SerializeField]
+    private Vector3 scale;    
+
+    public override void OnRecord(GameObject gameObject, Component component)
+    {
+        var t = component as Transform;
+        position = t.position;
+        rotation = t.rotation;
+        scale = t.localScale;
+    }
+
+    public override void OnReplay(GameObject gameObject, Component component)
+    {
+        gameObject.transform.position = position;
+        gameObject.transform.rotation = rotation;
+        gameObject.transform.localScale = scale;
+    }
+}
+
+
