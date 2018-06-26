ce567440e9072d56e4f19bf5bfb28399e6e43bad
Neil Devine <neil@unity3d.com>
2018-06-26 16:40:06 +0200

    animator recordable

---
+++ Assets/Scripts/Recordables/AnimatorRecordable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Recordables
+{
+	public class AnimatorRecordable : Recordable<Animator>
+	{
+		[SerializeField] private List<string> LayerNames = new List<string>();
+		
+		private MethodInfo mi;
+		public AnimatorRecordable()
+		{
+			mi = typeof(Animator).GetMethod("GetAnimatorStateName", BindingFlags.NonPublic | BindingFlags.Instance);
+		}
+		public override bool OnRecord(Recordable previous, Object source)
+		{
+			var animator = source as Animator;
+			var prev = previous as AnimatorRecordable;
+
+
+			for (int i = 0; i < animator.layerCount; i++)
+			{
+				var result = mi.Invoke(animator, new object[] {i, true});
+				LayerNames.Add(result as string);				
+			}
+			
+			if (prev != null && prev.LayerNames.SequenceEqual(LayerNames))
+				return false;
+			
+			return true;
+		}
+
+		public override void OnReplay(Object source)
+		{
+		}
+	}
+}
