9f19af3dcceb186ea6bce53982d43a23bc5f433f
Romain Failliot <romainf@unity3d.com>
2018-06-28 17:38:44 +0200

    Fix WASD keys and change camera pos

--- Assets/Scenes/CubeController.cs
+++ Assets/Scenes/CubeController.cs
@@ -8,22 +8,22 @@ public class CubeController : MonoBehaviour
 	{
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
-			transform.localPosition += Time.deltaTime * Speed * new Vector3(-1, 0, 0);
+			transform.localPosition += Time.deltaTime * Speed * -Vector3.right;
 		}
 		
 		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
 		{
-			transform.localPosition += Time.deltaTime * Speed * new Vector3(1, 0, 0);
+			transform.localPosition += Time.deltaTime * Speed * Vector3.right;
 		}
 
 		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
 		{
-			transform.localPosition += Time.deltaTime * Speed * new Vector3(1, 0, 0);
+			transform.localPosition += Time.deltaTime * Speed * Vector3.up;
 		}
 
 		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
 		{
-			transform.localPosition += Time.deltaTime * Speed * new Vector3(1, 0, 0);
+			transform.localPosition += Time.deltaTime * Speed * -Vector3.up;
 		}
 	}
 }
