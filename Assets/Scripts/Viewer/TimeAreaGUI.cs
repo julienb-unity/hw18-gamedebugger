9fb6e14cf2f4508de677fbb4928d486dc22b665a
julienb <julienb@unity3d.com>
2018-06-28 12:36:59 +0200

    Improved performance of time area.

--- Assets/Scripts/Viewer/TimeAreaGUI.cs
+++ Assets/Scripts/Viewer/TimeAreaGUI.cs
@@ -9,43 +9,52 @@ namespace GameDebugger
         object m_TimeArea;
         Rect m_LastRect;
 
-        MethodInfo m_BeginView;
-        MethodInfo m_EndView;
-        MethodInfo m_DrawTimeRuler;
         PropertyInfo m_TimeAreaRect;
-        MethodInfo m_TimeToPixel;
-        MethodInfo m_PixelToTime;
-        MethodInfo m_SetShownRange;
         PropertyInfo m_RangeLock;
 
+        Func<float, Rect, float> m_TimeToPixelDel;
+        Func<float, Rect, float> m_PixelToTimeDel;
+        Action m_BeginViewDel;
+        Action m_EndViewDel;
+        Action<Rect, float> m_DrawDel;
+        Action<float, float> m_SetShowRangeDel;
+        
         public TimeAreaGUI()
         {
             //Since the TimeArea is private, we need to get access to it by reflection, 
             var assembly = typeof(UnityEditor.Editor).Assembly;
             var timeAreaType = assembly.GetType("UnityEditor.TimeArea");
             m_TimeArea = Activator.CreateInstance(timeAreaType, new object[]{true, true, false});
-            m_BeginView = timeAreaType.GetMethod("BeginViewGUI");
-            m_EndView = timeAreaType.GetMethod("EndViewGUI");
+            var beginView = timeAreaType.GetMethod("BeginViewGUI");
+            var endView = timeAreaType.GetMethod("EndViewGUI");
             var signature = new[] {typeof (Rect), typeof (float)};
-            m_DrawTimeRuler = timeAreaType.GetMethod("TimeRuler", signature);
+            var drawTimeRuler = timeAreaType.GetMethod("TimeRuler", signature);
             m_TimeAreaRect = timeAreaType.GetProperty("rect");
             var timeAreaHRangeMin = timeAreaType.GetProperty("hRangeMin");
             timeAreaHRangeMin.SetValue(m_TimeArea, 0.0f, null);
-            m_TimeToPixel = timeAreaType.GetMethod("TimeToPixel", new[] {typeof(float), typeof(Rect)});
-            m_PixelToTime = timeAreaType.GetMethod("PixelToTime", new[] {typeof(float), typeof(Rect)});
-            m_SetShownRange = timeAreaType.GetMethod("SetShownHRange", new[]{typeof(float), typeof(float)});
-            m_SetShownRange.Invoke(m_TimeArea, new object[] {0.0f, (float)1});
+            var timeToPixel = timeAreaType.GetMethod("TimeToPixel", new[] {typeof(float), typeof(Rect)});
+            var pixelToTime = timeAreaType.GetMethod("PixelToTime", new[] {typeof(float), typeof(Rect)});
+            var setShownRange = timeAreaType.GetMethod("SetShownHRange", new[]{typeof(float), typeof(float)});
             m_RangeLock = timeAreaType.GetProperty("hRangeLocked");
-        }
 
+            m_PixelToTimeDel = (Func<float, Rect, float>) Delegate.CreateDelegate(typeof(Func<float, Rect, float>), m_TimeArea, pixelToTime);
+            m_TimeToPixelDel = (Func<float, Rect, float>) Delegate.CreateDelegate(typeof(Func<float, Rect, float>), m_TimeArea, timeToPixel);
+            m_BeginViewDel = (Action)Delegate.CreateDelegate(typeof(Action), m_TimeArea, beginView);
+            m_EndViewDel = (Action)Delegate.CreateDelegate(typeof(Action), m_TimeArea, endView);
+            m_DrawDel = (Action<Rect, float>)Delegate.CreateDelegate(typeof(Action<Rect, float>), m_TimeArea, drawTimeRuler);
+            m_SetShowRangeDel = (Action<float, float>)Delegate.CreateDelegate(typeof(Action<float, float>), m_TimeArea, setShownRange);
+            
+            m_SetShowRangeDel(0.0f, 1);
+        }
+        
         public float TimeToPixel(float time)
         {
-            return (float)m_TimeToPixel.Invoke(m_TimeArea, new object[]{time, m_LastRect});
+            return m_TimeToPixelDel(time, m_LastRect);
         }
 
         public float PixelToTime(float pixel)
         {
-            return (float)m_PixelToTime.Invoke(m_TimeArea, new object[]{ pixel, m_LastRect});
+            return m_PixelToTimeDel(pixel, m_LastRect);
         }
         
         /*
@@ -69,9 +78,9 @@ namespace GameDebugger
             
             m_LastRect = rect;
             m_TimeAreaRect.SetValue(m_TimeArea, rect, null);
-            m_BeginView.Invoke(m_TimeArea, null);
-            m_DrawTimeRuler.Invoke(m_TimeArea, new object[] {rect, 60.0f});
-            m_EndView.Invoke(m_TimeArea, null);
+            m_BeginViewDel();
+            m_DrawDel(rect, 60.0f);
+            m_EndViewDel();
 
             if (GameDebuggerRecorder.IsRecording)
             {
@@ -79,7 +88,7 @@ namespace GameDebugger
                 if (GameDebuggerDatabase.NumFrameRecords > 0)
                     maxTime = GameDebuggerDatabase.GetRecords(GameDebuggerDatabase.NumFrameRecords - 1).time;
                 maxTime = Math.Max(5.0f, maxTime);
-                m_SetShownRange.Invoke(m_TimeArea, new object[] { maxTime -10.0f, maxTime });
+                m_SetShowRangeDel(maxTime -10.0f, maxTime);
                 m_RangeLock.SetValue(m_TimeArea, true, null);
             }
             else
