37fa883e290604b77b1ab1b92c0324252a7be057
Romain Failliot <romainf@unity3d.com>
2018-06-28 17:34:08 +0200

    Add Up/Down and WASD keys in simple scene

--- Assets/Scenes/CubeController.cs
+++ Assets/Scenes/CubeController.cs
@@ -6,12 +6,22 @@ public class CubeController : MonoBehaviour
 
 	void Update () 
 	{
-		if (Input.GetKey(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
 			transform.localPosition += Time.deltaTime * Speed * new Vector3(-1, 0, 0);
 		}
 		
-		if (Input.GetKey(KeyCode.RightArrow))
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			transform.localPosition += Time.deltaTime * Speed * new Vector3(1, 0, 0);
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+		{
+			transform.localPosition += Time.deltaTime * Speed * new Vector3(1, 0, 0);
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
 		{
 			transform.localPosition += Time.deltaTime * Speed * new Vector3(1, 0, 0);
 		}
